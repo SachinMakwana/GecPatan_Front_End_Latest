e7b7dc87e7a540997a0f7b00ce2dfe3ef3ce7b4c
hetchauhan <hetchauhan555@gmail.com>
2022-09-13 13:43:50 +0530

    init

---
+++ GECP_FRONTEND_NET_CORE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GECP_FRONTEND_NET_CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
