d8d95971ca17324a2e8410575a3b676c72990067
Nishika Thakur <86481737+Nishika4918@users.noreply.github.com>
2022-09-13 14:53:14 +0530

    about us page

---
+++ GECP_FRONTEND_NET_CORE/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GECP_FRONTEND_NET_CORE.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult VisionMission()
+        {
+            return View();
+        }
+        public IActionResult Infrastructure()
+        {
+            return View();
+        }
+        public IActionResult Principal()
+        {
+            return View();
+        }
+        public IActionResult Overview()
+        {
+            return View();
+        }
+    }
+}
