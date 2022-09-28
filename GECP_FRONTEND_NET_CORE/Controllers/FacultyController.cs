fb3932ca387f8be5970a8c3880d4a909b49ea714
ADITYA PATEL <adityagpatel59@gmail.com>
2022-09-28 15:00:39 +0530

    event gallery faculty page ready

--- GECP_FRONTEND_NET_CORE/Controllers/FacultyController.cs
+++ GECP_FRONTEND_NET_CORE/Controllers/FacultyController.cs
@@ -1,28 +1,137 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BACKEND_HTML_DOT_NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace GECP_FRONTEND_NET_CORE.Controllers
 {
     public class FacultyController : Controller
     {
+        private string apiBaseUrl = "https://localhost:44374/api";
+        HttpClient hc = new HttpClient();
+        private static List<FacultyDetailsVM> facultyList = new List<FacultyDetailsVM>();
+
+        RestClient client;
+        public FacultyController()
+        {
+
+            client = new RestClient(apiBaseUrl);
+        }
         public IActionResult ComputerScience()
         {
-            return View();
+            var restRequest5 = new RestRequest("/GetAllFacultyDetails", Method.Get);
+            restRequest5.AddHeader("Accept", "application/json");
+            restRequest5.RequestFormat = DataFormat.Json;
+
+            RestResponse response5 = client.Execute(restRequest5);
+
+            var content5 = response5.Content;
+            if (content5 != null)
+            {
+                var user = JsonConvert.DeserializeObject<ServiceResponse<List<FacultyDetailsVM>>>(content5);
+                facultyList = user.data;
+                foreach (var data in facultyList)
+                {
+                    data.Image = "https://localhost:44374/" + data.Image;
+                }
+            }
+
+            return View(facultyList);
         }
         public IActionResult Mechanical()
         {
-            return View();
+            var restRequest5 = new RestRequest("/GetAllFacultyDetails", Method.Get);
+            restRequest5.AddHeader("Accept", "application/json");
+            restRequest5.RequestFormat = DataFormat.Json;
+
+            RestResponse response5 = client.Execute(restRequest5);
+
+            var content5 = response5.Content;
+            if (content5 != null)
+            {
+                var user = JsonConvert.DeserializeObject<ServiceResponse<List<FacultyDetailsVM>>>(content5);
+                facultyList = user.data;
+                foreach (var data in facultyList)
+                {
+                    data.Image = "https://localhost:44374/" + data.Image;
+                }
+            }
+
+            return View(facultyList);
         }
         public IActionResult Electrical()
         {
-            return View();
+            var restRequest5 = new RestRequest("/GetAllFacultyDetails", Method.Get);
+            restRequest5.AddHeader("Accept", "application/json");
+            restRequest5.RequestFormat = DataFormat.Json;
+
+            RestResponse response5 = client.Execute(restRequest5);
+
+            var content5 = response5.Content;
+            if (content5 != null)
+            {
+                var user = JsonConvert.DeserializeObject<ServiceResponse<List<FacultyDetailsVM>>>(content5);
+                facultyList = user.data;
+                foreach (var data in facultyList)
+                {
+                    data.Image = "https://localhost:44374/" + data.Image;
+                }
+            }
+
+            return View(facultyList);
         }
         public IActionResult ElectronicsCommunication()
         {
-            return View();
+            var restRequest5 = new RestRequest("/GetAllFacultyDetails", Method.Get);
+            restRequest5.AddHeader("Accept", "application/json");
+            restRequest5.RequestFormat = DataFormat.Json;
+
+            RestResponse response5 = client.Execute(restRequest5);
+
+            var content5 = response5.Content;
+            if (content5 != null)
+            {
+                var user = JsonConvert.DeserializeObject<ServiceResponse<List<FacultyDetailsVM>>>(content5);
+                facultyList = user.data;
+                foreach (var data in facultyList)
+                {
+                    data.Image = "https://localhost:44374/" + data.Image;
+                }
+            }
+
+            return View(facultyList);
         }
         public IActionResult Civil()
         {
-            return View();
+            var restRequest5 = new RestRequest("/GetAllFacultyDetails", Method.Get);
+            restRequest5.AddHeader("Accept", "application/json");
+            restRequest5.RequestFormat = DataFormat.Json;
+
+            RestResponse response5 = client.Execute(restRequest5);
+
+            var content5 = response5.Content;
+            if (content5 != null)
+            {
+                var user = JsonConvert.DeserializeObject<ServiceResponse<List<FacultyDetailsVM>>>(content5);
+                facultyList = user.data;
+                foreach (var data in facultyList)
+                {
+                    data.Image = "https://localhost:44374/" + data.Image;
+                }
+            }
+
+            return View(facultyList);
+        }
+        public IActionResult FacultyView(int id)
+        {
+            foreach(var faculty in facultyList)
+            {
+                if(faculty.Id == id)
+                {
+                    return View(faculty);
+                }
+            }
+            return NotFound();
         }
     }
 }
--- GECP_FRONTEND_NET_CORE/Controllers/GalleryController.cs
+++ GECP_FRONTEND_NET_CORE/Controllers/GalleryController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BACKEND_HTML_DOT_NET.Models;
+using GECP_FRONTEND_NET_CORE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace GECP_FRONTEND_NET_CORE.Controllers
 {
     public class GalleryController : Controller
     {
+        private string apiBaseUrl = "https://localhost:44374/api";
+        HttpClient hc = new HttpClient();
+        private static List<GalleryVM> galleryList = new List<GalleryVM>();
+
+        RestClient client;
+        public GalleryController()
+        {
+            client = new RestClient(apiBaseUrl);
+        }
         public IActionResult Gallery()
         {
-            return View();
+            var restRequest6 = new RestRequest("/GetAllGalleryDetails", Method.Get);
+            restRequest6.AddHeader("Accept", "application/json");
+            restRequest6.RequestFormat = DataFormat.Json;
+
+            RestResponse response6 = client.Execute(restRequest6);
+
+            var content6 = response6.Content;
+            if (content6 != null)
+            {
+                var user = JsonConvert.DeserializeObject<ServiceResponse<List<GalleryVM>>>(content6);
+                galleryList = user.data;
+                foreach (var data in galleryList)
+                {
+                    data.Image = "https://localhost:44374/" + data.Image;
+                }
+            }
+            return View(galleryList);
         }
     }
 }
